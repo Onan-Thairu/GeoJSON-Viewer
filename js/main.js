0483d61043418b94bb0addf9d0a873c916fa1a0f
Onan Thairu <thairuonan@gmail.com>
2022-03-10 13:00:31 +0300

    Change leaflet provider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var map = L.map("map").setView([0, 0], 1); // Setting map extent
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: "abcd",
-    maxZoom: 19
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 var layers = L.layerGroup().addTo(map);
